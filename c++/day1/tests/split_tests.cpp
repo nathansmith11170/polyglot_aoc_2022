6b31dffb5f53f064dca95138ba20b78ec753aafd
nathansmith11170 <smith.11170@outlook.com>
2023-05-16 17:37:30 -0400

    move C++ toolchain to clang, start using clang-format, enable some compiler warnings on debug profile

--- c++/day1/tests/split_tests.cpp
+++ c++/day1/tests/split_tests.cpp
@@ -1,52 +1,52 @@
-#include <gtest/gtest.h>
 #include <day1/core_func.h>
+#include <gtest/gtest.h>
 
 TEST(SplitTest, SplitStringByComma) {
-    std::string input_str = "apple,banana,orange";
-    const char* delimiter = ",";
-    std::vector<std::string> expected_tokens = {"apple", "banana", "orange"};
+  std::string input_str = "apple,banana,orange";
+  const char *delimiter = ",";
+  std::vector<std::string> expected_tokens = {"apple", "banana", "orange"};
 
-    std::vector<std::string> actual_tokens = split(input_str, delimiter);
+  std::vector<std::string> actual_tokens = split(input_str, delimiter);
 
-    EXPECT_EQ(expected_tokens, actual_tokens);
+  EXPECT_EQ(expected_tokens, actual_tokens);
 }
 
 TEST(SplitTest, SplitStringBySpace) {
-    std::string input_str = "apple banana orange";
-    const char* delimiter = " ";
-    std::vector<std::string> expected_tokens = {"apple", "banana", "orange"};
+  std::string input_str = "apple banana orange";
+  const char *delimiter = " ";
+  std::vector<std::string> expected_tokens = {"apple", "banana", "orange"};
 
-    std::vector<std::string> actual_tokens = split(input_str, delimiter);
+  std::vector<std::string> actual_tokens = split(input_str, delimiter);
 
-    EXPECT_EQ(expected_tokens, actual_tokens);
+  EXPECT_EQ(expected_tokens, actual_tokens);
 }
 
 TEST(SplitTest, SplitStringByDoubleSpace) {
-    std::string input_str = "apple  banana  orange";
-    const char* delimiter = "  ";
-    std::vector<std::string> expected_tokens = {"apple", "banana", "orange"};
+  std::string input_str = "apple  banana  orange";
+  const char *delimiter = "  ";
+  std::vector<std::string> expected_tokens = {"apple", "banana", "orange"};
 
-    std::vector<std::string> actual_tokens = split(input_str, delimiter);
+  std::vector<std::string> actual_tokens = split(input_str, delimiter);
 
-    EXPECT_EQ(expected_tokens, actual_tokens);
+  EXPECT_EQ(expected_tokens, actual_tokens);
 }
 
 TEST(SplitTest, SplitStringByDoubleNewline) {
-    std::string input_str = "apple\n\nbanana\n\norange";
-    const char* delimiter = "\n\n";
-    std::vector<std::string> expected_tokens = {"apple", "banana", "orange"};
+  std::string input_str = "apple\n\nbanana\n\norange";
+  const char *delimiter = "\n\n";
+  std::vector<std::string> expected_tokens = {"apple", "banana", "orange"};
 
-    std::vector<std::string> actual_tokens = split(input_str, delimiter);
+  std::vector<std::string> actual_tokens = split(input_str, delimiter);
 
-    EXPECT_EQ(expected_tokens, actual_tokens);
+  EXPECT_EQ(expected_tokens, actual_tokens);
 }
 
 TEST(SplitTest, SplitStringByNewline) {
-    std::string input_str = "\napple\nbanana\norange\n";
-    const char* delimiter = "\n";
-    std::vector<std::string> expected_tokens = {"apple", "banana", "orange"};
+  std::string input_str = "\napple\nbanana\norange\n";
+  const char *delimiter = "\n";
+  std::vector<std::string> expected_tokens = {"apple", "banana", "orange"};
 
-    std::vector<std::string> actual_tokens = split(input_str, delimiter);
+  std::vector<std::string> actual_tokens = split(input_str, delimiter);
 
-    EXPECT_EQ(expected_tokens, actual_tokens);
+  EXPECT_EQ(expected_tokens, actual_tokens);
 }
